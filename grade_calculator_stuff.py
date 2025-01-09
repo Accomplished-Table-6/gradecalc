0ae82601b6f8a8331d4a40947ab238f6d7313622
Accomplished-Table-6 <edwinsalisbury83@gmail.com>
2025-01-09 11:43:08 -0600

    Completely rewrote it

--- grade_calculator_stuff.py
+++ grade_calculator_stuff.py
@@ -1,19 +1,38 @@
 import numpy as np
 
-current_grade = float(input("Enter current grade in class: "))
-test_grade_string = str(input("Enter each test grade followed by a space or just enter the average grade of tests: "))
-a = test_grade_string.split()
-test_grade_list = [float(item) for item in a]
+grade_list = []
 
-predicted_exam_grade = np.mean(test_grade_list)
+def fsg(semester_grade_1,semester_grade_2, exam_grade,sem_num): # fsg stands for first semester grade.
+    if sem_num == 1:
+        return ((semester_grade_1*0.8) + (exam_grade*0.2))
+    elif sem_num == 2:
+        return ((semester_grade_1*0.4) + (semester_grade_2*0.4) + (exam_grade*0.2))
 
-def fgradecalc(exam_grade,semester_grade):
-    fg = (exam_grade*0.20) + (semester_grade*0.80)
-    return round(fg,2)
+s_num = int(input("Do you want to calculate grade after final exam (enter 1) or midterm (enter 2)?: "))
+if s_num == 1:
+    secq_grade = float(input("Please enter your current 2nd quarter grade: "))
+    test_numbers = int(input("How many tests have you taken this semester?: "))
+    for i in range(test_numbers):
+        test_grade = float(input(f"Please input numerical grade for test number {i+1}: "))
+        grade_list.append(test_grade)
+    test_grade_average = np.mean(grade_list)
+    print(f"Your current grade is a {secq_grade}.")
+    print(f"Based on previous test grades, your average test grade is a {round(test_grade_average,2)}.")
+    print(f"Following this trajectory, your first semester grade would probably be a {round(fsg(secq_grade,0,test_grade_average,1),2)}.")
+    print(f"The maximum grade you could have in the class at the end of the first semester would be a {round(fsg(secq_grade,0,100.0,1),2)}.")
+    print(f"If you got a 0 on the exam your grade in the class at the end of the first semester would be a {round(fsg(secq_grade,0,0,1),2)}.")
+elif s_num == 2:
+    sem_grade_1 = float(input("Please enter your 1st semester grade: "))
+    forq_grade = float(input("Please enter your current 4th quarter grade: "))
+    test_numbers = int(input("How many tests have you taken this semester?: "))
+    for i in range(test_numbers):
+        test_grade = float(input(f"Please input numerical grade for test number {i+1}: "))
+        grade_list.append(test_grade)
+    test_grade_average = np.mean(grade_list)
+    print(f"Your current grade is a {forq_grade}.")
+    print(f"Based on previous test grades, your average test grade is a {round(test_grade_average,2)}.")
+    print(f"Following this trajectory, your final grade would probably be a {round(fsg(forq_grade,sem_grade_1,test_grade_average,2),2)}.")
+    print(f"The maximum final grade you could have in the class would be a {round(fsg(forq_grade,sem_grade_1,100.0,2),2)}.")
+    print(f"If you got a 0 on the exam your final grade in the class would be a {round(fsg(forq_grade,sem_grade_1,0,2),2)}.")
 
-print(f"Your average test grade is {round(predicted_exam_grade,2)}")
-print(f"Based on this your final grade will likely be {fgradecalc(predicted_exam_grade,current_grade)}")
-print(f"If your teacher marks the final exam as a 0, then your final grade will be {fgradecalc(0.0,current_grade)}")
-print(f"If you got a 50 on the final exam, your final grade would be would be {fgradecalc(50.0,current_grade)}")
-print(f"If you got a 75 on the final exam, your final grade would be would be {fgradecalc(75.0,current_grade)}")
-print(f"The highest your final grade could be is {fgradecalc(100.0,current_grade)}")
+
